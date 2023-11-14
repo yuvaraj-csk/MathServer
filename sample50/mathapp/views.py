f930e40e5ed3bf563296016bdf69c0d7f9119532
yuvaraj-csk <sandhiyayuvaraj08@gmail.com>
2023-11-14 08:57:53 +0530

    Success

---
+++ sample50/mathapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def Squareprism(request):
+    context={}
+    context['area'] = "0"
+    context['l'] = "0"
+    context['b'] = "0"
+    if request.method == 'POST':
+        print("POST method is used")
+        l = request.POST.get('length','0')
+        b = request.POST.get('breadth','0')
+        print('request=',request)
+        print('Length=',l)
+        print('Breadth=',b)
+        area = 2*(int(l)**2) + 4*int(l)*int(b)
+        context['area'] = area
+        context['l'] = l
+        context['b'] = b
+        print('Area=',area)
+    return render(request,'mathapp/math.html',context)
